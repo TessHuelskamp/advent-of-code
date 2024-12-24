143ea283e31e2320a754a188e7e889e553b516c7
Tess Huelskamp <tess@huelskamp.io>
2024-12-23 20:17:46 -0500

    p2

--- 2024/16/solution.py
+++ 2024/16/solution.py
@@ -28,7 +28,79 @@ class Board:
   
   def partOne(self):
     return self.runShortestPath()
+  
+  def partTwo(self):
+    if not self.lowestVal or len(self.lowestVal) == 0:
+      # populate dir
+      self.runShortestPath()
+
+    # too high: 544
+
+
+    ei, ej = self.find("E")
+    minDir = self.minDir(ei, ej)
+
+    value = self.lowestVal[self.key(ei, ej, minDir)]
+
+    seen = set()
+    added = {self.key(ei, ej, minDir)}
+    toVisit = [[value, ei, ej, minDir]]
+
+    while toVisit:
+      val, i, j, dir = toVisit[0]
+      toVisit = toVisit[1:]
+      seen.add(self.keyy(i, j))
+
+      for dirr in ["N", "S", "E", "W"]:
+        try:
+          vall = self.lowestVal[self.key(i, j, dirr)]
+        except KeyError:
+          continue
+
+        aKey = self.key(i, j, dirr)
+
+        if val-1000 == vall or val-1 == vall and aKey not in added:
+          added.add(aKey)
+          toVisit.append([vall, i, j, dirr])
+
+      
+      for n in self.neighbors(i, j):
+        ii, jj = n
+        for dirr in ["N", "S", "E", "W"]:
+          try:
+            vall = self.lowestVal[self.key(ii, jj, dirr)]
+          except KeyError:
+            continue
+
+          # An optimal path goes through this value if we can back-track
+          # A solution that is 1 or 1000 less than this thing
+
+
+          aKey = self.key(ii, jj, dirr)
+
+          if val-1000 == vall or val-1 == vall and aKey not in added:
+            added.add(aKey)
+            toVisit.append([vall, ii, jj, dirr])
+
+      # possibleVals = anything near that is 1 or 1000 less than the val
+
+    for i, line in enumerate(self.board):
+      for j, val in enumerate(line):
+        if self.keyy(i, j) in seen:
+          print("O", end="")
+        else:
+          print(self.board[i][j], end="")
+        
+      print("")
+
     
+
+    return len(seen)
+    
+
+    
+  def keyy(self, x, y):
+    return str(x)+"-"+str(y)
   def key(self, x, y, dir):
     return str(x)+"-"+str(y)+"-"+dir
   
@@ -67,24 +139,26 @@ class Board:
       return ii, jj
     else: return None, None
 
+  
+
   def runShortestPath(self):
     # clear out shortest-steps
-    lowestVal = dict()
+    self.lowestVal = dict()
 
     # value, i, j, direction
     toVisit = [[0, *self.find("S"), "E"]]
 
     while toVisit:
-      value, x, y, dir= toVisit[0]
+      value, x, y, dir=toVisit[0]
       toVisit = toVisit[1:]
 
       if self.blocked(x, y): continue
 
       key = self.key(x, y, dir)
-      currentVal = lowestVal.get(key)
+      currentVal = self.lowestVal.get(key)
       
       if currentVal is None or value < currentVal:
-        lowestVal[key]=value
+        self.lowestVal[key]=value
 
         xx, yy = self.getMove(x, y, dir)
         if xx:
@@ -97,16 +171,27 @@ class Board:
       # short toVisit based off of first val
       toVisit.sort(key=lambda x: x[0])
 
-    vals = list()
     ei, ej = self.find("E")
 
+    minDir = self.minDir(ei, ej)
+
+    return self.lowestVal[self.key(ei, ej, minDir)]
+  
+  
+  def minDir(self, ei, ej):
+    lowestval = 999_999_999_999
+    lowestDir = None
     for dir in ["N", "S", "E", "W"]:
       key = self.key(ei, ej, dir)
-      stuff = lowestVal[key]
-      if stuff : vals.append(stuff)
+      currentVal = self.lowestVal[key]
+      if currentVal < lowestval:
+        lowestval = currentVal
+        lowestDir = dir
+
+    return lowestDir
 
 
-    return min(vals)
+
   
   def printBoard(self):
     for line in self.board:
@@ -117,6 +202,9 @@ b = Board(board)
 
 
 print(b.partOne())
+p2 = b.partTwo()
+# manual inspection of board to fix algo errors :)
 
-# b.printBoard()
-# print(b.partTwo())
+print(p2 -(5+3+7))
+
+
